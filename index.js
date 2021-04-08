c52240d18062398b313d055cc1de49b4ea991906
tuanlong1202 <tuanlong1202@gmail.com>
2021-04-08 12:31:51 -0400

    Removing inserting altering DOM

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+let pMain = document.getElementById("main");
+pMain.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+
+document.body.appendChild(newHeader);
